bbcf3751bbf1454fd52c3d7778e55aebd02bc010
Hamid Wahyudin <hamidwahyudin1987@gmail.com>
2017-08-02 15:37:36 +0700

    ADD index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Grab Website PHP by Hamid Wahyudin</title>
+</head>
+
+<body>
+	<ul>
+		<li><a href="grab_bca.php">Kurs BCA</a></li>
+		<li><a href="grab_bi.php">Kurs BI</a></li>
+		<li><a href="grab_kurs_mandiri.php">Kurs Mandiri</a></li>
+		<li><a href="grab_provinsi_wikipedia.php">Provinsi Wikipedia</a></li>
+	</ul>
+</body>
+
+</html> 
