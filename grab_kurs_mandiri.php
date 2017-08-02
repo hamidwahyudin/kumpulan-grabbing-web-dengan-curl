9e04e5e25525df19f0efc540602912531818a4c8
Hamid Wahyudin <hamidwahyudin1987@gmail.com>
2017-08-02 15:23:56 +0700

    Perbaikan grab bank mandiri

--- grab_kurs_mandiri.php
+++ grab_kurs_mandiri.php
@@ -5,13 +5,13 @@
 
 
 function fungsiCurl($url){
-     $data = curl_init();
-     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
-     curl_setopt($data, CURLOPT_URL, $url);
-	 curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
-     $hasil = curl_exec($data);
-     curl_close($data);
-     return $hasil;
+    $data = curl_init();
+    curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($data, CURLOPT_URL, $url);
+	curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
+    $hasil = curl_exec($data);
+    curl_close($data);
+    return $hasil;
 }
 $url = fungsiCurl('http://www.bankmandiri.co.id/resource/kurs.asp');
 $pecah_last_update_1 = explode('<p class="catatan">', $url);
